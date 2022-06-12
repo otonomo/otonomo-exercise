1799e1be962fb4ea4c7205af7cc1798797a05d4c
orenhd <orenhd123@gmail.com>
2022-06-12 11:38:50 +0300

    app-panes html - in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import './App.css'
-import React from 'react'
+import './App.scss'
+import React, { PureComponent } from 'react'
 import generateCarData from './api/data-generator'
-import createStreamerFrom from './api/streamer'
-import logo from './logo.svg'
+import createStreamerFrom, { Streamer } from './api/streamer'
+import { SubscribedVin } from './App.types';
+import Input from './components/Input';
+import Button from './components/Button';
+import Checkbox from './components/Checkbox';
 
-const App = () => {
+const STREAMERS_CACHE: {[vin: string]: Streamer } = {};
+
+interface Props {}
+
+interface State {
+  subscribedVinsMap: {[vin:string]: SubscribedVin};
+}
+
+class App extends PureComponent<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      subscribedVinsMap:{},
+    }
+  }
+  /*
   const [carData, setCarData] = React.useState({})
   const streamer = React.useMemo(() => createStreamerFrom(() => generateCarData('12345678901234567')), [])
 
@@ -26,5 +45,29 @@ const App = () => {
     </div>
   )
 }
+*/
 
-export default App
+
+  render() {
+
+    const { subscribedVinsMap } = this.state;
+    return <React.Fragment>
+      <div className="app-pane app-left-pane">
+        <div className="app-left-pane-top-bar">
+          <Input></Input>
+          <Button>Add</Button>
+        </div>
+        <ul className="app-left-pane-vins-list">
+          {Object.values(subscribedVinsMap).map((subVin) => <li key={subVin.id}>
+            <Checkbox>{subVin.id}</Checkbox>
+          </li>)}
+        </ul>
+      </div>
+      <div className="app-pane app-right-pane">
+
+      </div>
+    </React.Fragment>
+  }
+}
+
+export default App;
